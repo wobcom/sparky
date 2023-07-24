604eecbe6c55d6ce265746574a4ee54caf01dc8a
Jennifer Graul <jennifer.graul@wobcom.de>
2023-07-24 15:17:56 +0200

    iperf3 exporter: fix iperf3 path

--- modules/iperf3-exporter/iperf3.nix
+++ modules/iperf3-exporter/iperf3.nix
@@ -12,7 +12,6 @@ in
       ExecStart = ''
         ${pkgs.prometheus-iperf3-exporter}/bin/iperf3-exporter \
           -web.listen-address ${cfg.listenAddress}:${toString cfg.port} \
-          -iperf3.path ${pkgs.iperf3}/bin/iperf3 \
           ${concatStringsSep " \\\n  " cfg.extraFlags}
       '';
     };
