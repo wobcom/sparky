2fe232d6ce78631f24736b3bea45354d40017d17
Jennifer Graul <jennifer.graul@wobcom.de>
2023-08-29 13:24:18 +0200

    flake: use unstable nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS Modules for a network reliability and connectivity testing infrastructure";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
