62a15b9f0e7b3b7f3c301cc5e4ca58c0a148d49a
fiona <git@unternet.eu>
2026-01-19 14:59:00 +0100

    update from nixos 25.05 to 25.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS Modules for a network reliability and connectivity testing infrastructure";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
